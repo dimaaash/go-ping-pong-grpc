bca10569ae90370333c8cddf61c225b28d8e8070
Dmitry Kuznetsov <dimaaash@gmail.com>
2024-05-02 21:04:50 -0400

    updating

--- ping/pkg/service/ping.go
+++ ping/pkg/service/ping.go
@@ -1,15 +1,15 @@
 package service
 
-import (
-	pb "github.com/dimaaash/go-ping-pong-grpc/ping"
-)
+// import (
+// 	pb "github.com/dimaaash/go-ping-pong-grpc/ping"
+// )
 
 // type PingService interface {
 // 	Ping() string
 // }
 
 type PingServer struct {
-	pb.UnimplementedHelloWorldServiceServer
+	// pb.UnimplementedHelloWorldServiceServer
 }
 
 func (s *PingServer) Ping() string {
