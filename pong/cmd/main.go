da1c0ff039017bf55797adc99d8b5a26e372b363
Dmitry Kuznetsov <dimaaash@gmail.com>
2024-05-03 16:00:58 -0400

    updating

--- pong/cmd/main.go
+++ pong/cmd/main.go
@@ -2,9 +2,30 @@ package main
 
 import (
 	"log"
+	"net"
+
+	"github.com/dimaaash/go-ping-pong-grpc/pong/pkg/service"
+	pongpb "github.com/dimaaash/go-ping-pong-grpc/protos/gen/pong"
+	"google.golang.org/grpc"
 )
 
 func main() {
 
 	log.Println("Pong Server starting...")
+
+	lis, err := net.Listen("tcp", "50004")
+
+	if err != nil {
+		log.Fatalln("Failed to listing:", err)
+	}
+
+	grpcServer := grpc.NewServer()
+
+	s := service.PongServer{}
+
+	pongpb.RegisterPongServiceServer(grpcServer, &s)
+
+	if err := grpcServer.Serve(lis); err != nil {
+		log.Fatalln("Failed to serve:", err)
+	}
 }
